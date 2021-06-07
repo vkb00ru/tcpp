ab8f7175547cb6a925872b9126a3afd14732accb
valera <vkb00@mail.ru>
2021-06-08 01:00:07 +0300

    create class ZAKAZ

--- TCPP/Source.cpp
+++ TCPP/Source.cpp
@@ -5,6 +5,26 @@
 
 
 using namespace std;
+
+class Zakaz
+{
+	int id;
+	int idClient;
+	int idTov[5];
+	int countTov = 0;
+	string status;
+
+public:
+	string getStatus() { return status; };
+	int getidTov(int i) { return idTov[i]; };
+	int getClient() { return idClient; };
+	int getcountTov() { return countTov; };
+	int getid() { return id; };
+	void setStatus(string s) { status = s; };
+	void setidTov(int i) { idTov[countTov] = i; countTov++; };
+	void setidClient(int i) { idClient = i; };
+	void setid(int i) { id = i; };
+};
 void main()
 {
 	system("pause");
